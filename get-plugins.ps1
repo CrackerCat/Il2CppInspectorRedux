3e5622ac3782aa84b3546a4b7d91bd05e42d8760
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-12-01 10:45:09 +0100

    Fix get-plugins.ps1

--- get-plugins.ps1
+++ get-plugins.ps1
@@ -2,7 +2,7 @@
 # All rights reserved.
 
 # Download the latest set of plugins to a temporary file, extract the archive then delete it
-$temp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } –PassThru
+$temp = New-TemporaryFile | Rename-Item -NewName { $_ -replace 'tmp$', 'zip' } -PassThru
 wget -OutFile $temp https://github.com/djkaty/Il2CppInspectorPlugins/releases/latest/download/plugins.zip
 Expand-Archive -Path $temp -DestinationPath $pwd -Force
 del $temp
